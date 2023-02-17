46b58b24ed7f020b44cd32e45b625823e80f5f91
Augusto Rodrigues <augustofeliperodrigues1999@gmail.com>
2023-02-17 17:28:41 -0300

    feat(backend): Added backend directory

---
+++ backend/src/main/java/com/augusto/crudspring/controller/CourseController.java
@@ -0,0 +1,39 @@
+package com.augusto.crudspring.controller;
+
+import com.augusto.crudspring.model.Course;
+import com.augusto.crudspring.repository.CourseRepository;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/courses")
+public class CourseController {
+
+    private final CourseRepository courseRepository;
+
+    public CourseController(CourseRepository courseRepository) {
+        this.courseRepository = courseRepository;
+    }
+
+    @GetMapping
+    public List<Course> list() {
+        return this.courseRepository.findAll();
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Course> findById(@PathVariable Long id) {
+        return courseRepository.findById(id)
+                .map(record -> ResponseEntity.ok().body(record))
+                .orElse(ResponseEntity.notFound().build());
+    }
+
+    @PostMapping
+    @ResponseStatus( code = HttpStatus.ACCEPTED )
+    public Course save(@RequestBody Course course){
+        return this.courseRepository.save(course);
+    }
+
+}
